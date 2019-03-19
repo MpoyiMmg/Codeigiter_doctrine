088ac831f7dda01d023fbb2e8ef829b24ee230b5
= <=>
2019-03-19 15:07:08 +0200

    [FIX] fix Classes import

--- application/libraries/Doctrine.php
+++ application/libraries/Doctrine.php
@@ -1,5 +1,9 @@
 <?php
 
-    
+    use Doctrine\Common\ClassLoader,
+        Doctrine\ORM\Configuration,
+        Doctrine\ORM\EntityManager,
+        Doctrine\Common\Cache\ArrayCache,
+        Doctrine\DBAL\Logging\EchoSQLLogger;
 
 ?>
